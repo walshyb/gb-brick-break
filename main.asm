9ae2f722b779e6b49e3b2dfd8c5dc75c49f9d669
Brandon Walsh <bmwalshy@gmail.com>
2024-02-06 18:35:19 -0500

    Add paddle to screen

--- main.asm
+++ main.asm
@@ -32,8 +32,6 @@ CopyTiles:
   or a, c
   jp nz, CopyTiles
 
-  
-  
   ; Copy the tilemap
   ld de, Tilemap
   ld hl, $9800
@@ -47,13 +45,65 @@ CopyTilemap:
   or a, c
   jp nz, CopyTilemap
 
-; Turn the LCD on
-ld a, LCDCF_ON | LCDCF_BGON
+  ld a, 0
+  ld b, 160
+  ld hl, _OAMRAM
+
+  ; Copy the tile data
+  ld de, Paddle
+  ld hl, $8000
+  ld bc, PaddleEnd - Paddle
+CopyPaddle:
+  ld a, [de]
+  ld [hli], a
+  inc de
+  dec bc
+  ld a, b
+  or a, c
+  jp nz, CopyPaddle
+
+ClearOam:
+  ld [hli], a
+  dec b
+  jp nz, ClearOam
+
+  ld hl, _OAMRAM
+  ld a, 128 + 16
+  ld [hli], a
+  ld a, 16 + 8
+  ld [hli], a
+  ld a, 0
+  ld [hli], a
+  ld [hl], a
+
+; Turn the LCD on,
+; Enabling, LCD, BG Pallet, and Objects
+ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON
 ld [rLCDC], a
 
 ; During the first (blank) frame, initialize display registers
 ld a, %11100100
 ld [rBGP], a
+
+; Initialize object pallet
+ld a, %11100100
+ld [rOBP0], a
+
+Main:
+  ; Wait until it's *not* VBlank
+  ld a, [rLY]
+  cp 144
+  jp nc, Main
+WaitVBlank2:
+  ld a, [rLY]
+  cp 144
+  jp c, WaitVBlank2
+
+  ; Move the paddle one pixel to the right.
+  ld a, [_OAMRAM + 1]
+  inc a
+  ld [_OAMRAM + 1], a
+  jp Main
 
 Done:
   jp Done
@@ -293,3 +343,14 @@ Tilemap:
   db $04, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $08, $07, $03, $16, $17, $18, $19, $03, 0,0,0,0,0,0,0,0,0,0,0,0
   db $04, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $09, $07, $03, $03, $03, $03, $03, $03, 0,0,0,0,0,0,0,0,0,0,0,0
 TilemapEnd:
+
+Paddle:
+    dw `13333331
+    dw `30000003
+    dw `13333331
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+    dw `00000000
+PaddleEnd:
