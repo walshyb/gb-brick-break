3f6a2e6ec663711a2fa3228bfa4892bc9a3afb6b
Brandon Walsh <bmwalshy@gmail.com>
2024-02-08 12:41:02 -0500

    Start adding some structure

--- watch.sh
+++ watch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 compile() {
 	rgbasm -L -o main.o main.asm
+	rgbasm -L -o ./objects/paddle.o ./objects/paddle.asm
 	rgblink -o brickbreak.gb main.o
 	rgbfix -v -p 0xFF brickbreak.gb
 	rgblink -n brickbreak.sym main.o
 }
 
-fswatch -o main.asm | while read -r; do
+compile
+
+# Watch for changes in all .asm files
+fswatch -o */**.asm | while read -r; do
 	compile
 done
