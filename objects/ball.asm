64638dcbb27a4da18b9b811814d115daa6f72286
Brandon Walsh <bmwalshy@gmail.com>
2024-02-08 13:33:21 -0500

    Comments

--- objects/ball.asm
+++ objects/ball.asm
@@ -1,21 +1,27 @@
 SECTION "BALL", ROM0
 
-; Updates Ball Momentum
-; @param a: New Value of ball
+; Updates Ball Momentum Y
+; @param a: New value of ball
 UpdateBallMomentumY::
   ld [wBallMomentumY], a
 	ret
 
-; Updates Ball Momentum
-; @param a: New Value of ball
+; Updates Ball Momentum X
+; @param a: New value of ball
 UpdateBallMomentumX::
   ld [wBallMomentumX], a
 	ret
 
+; Get Ball Momentum X
+; Loads value into a
+; @param a: Current value of ball
 GetBallMomentumX::
 	ld a, [wBallMomentumX]
 	ret
 
+; Get Ball Momentum Y
+; Loads value into a
+; @param a: Current value of ball
 GetBallMomentumY::
 	ld a, [wBallMomentumY]
 	ret
